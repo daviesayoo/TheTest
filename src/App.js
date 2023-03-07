9d71c6983b8cfbc7eeadc64c4621a0e3f1204821
daviesayo <daviesayo14@gmail.com>
2023-03-07 15:44:52 -0600

    add start scripts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+/* *|MARCADOR_CURSOR|* */
+/* *|CURSOR_MARCADOR|* */
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { FaHome, FaCog, FaCode, FaLightbulb, FaMoon } from "react-icons/fa";
+
+
+// import Page1 from "./components/Page1";
+// import Page2 from "./components/Page2";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(
@@ -12,6 +18,8 @@ function App() {
   const [buttonCount, setButtonCount] = useState(0);
   const [inputValue, setInputValue] = useState("");
   const [messages, setMessages] = useState([]);
+
+  const [theme, setTheme] = useState("light");
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -69,11 +77,19 @@ function App() {
             </button>
           </div>
         </nav>
+        <div>
+
+        <div className="content"> 
+              {isMenuOpen && ( <aside className="menu"> <ul> <li> <Link to="/page1"> <FaHome /> {isMenuOpen}  
 
         </div>
-        
 
-              </Router>
-)};
+
+
+        </div>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
